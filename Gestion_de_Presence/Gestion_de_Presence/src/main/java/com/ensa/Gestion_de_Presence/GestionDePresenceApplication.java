f4b2ec4fc53833f00983657ce0ffff273e155699
l9irche <153326705+l9irche@users.noreply.github.com>
2025-03-24 17:07:29 +0000

    initialisation

---
+++ Gestion_de_Presence/Gestion_de_Presence/src/main/java/com/ensa/Gestion_de_Presence/GestionDePresenceApplication.java
@@ -0,0 +1,13 @@
+package com.ensa.Gestion_de_Presence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDePresenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDePresenceApplication.class, args);
+	}
+
+}
---
+++ Gestion_de_Presence/Gestion_de_Presence/src/test/java/com/ensa/Gestion_de_Presence/GestionDePresenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensa.Gestion_de_Presence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDePresenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
